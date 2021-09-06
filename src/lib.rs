cfc33bc8b89844aa671c2b3cb6f9c46a24ee5a13
Vignesh M <ms.vignesh31@gmail.com>
2021-09-07 00:24:16 +0530

    Defined interfaces and basic methods in the interface for cells

--- src/lib.rs
+++ src/lib.rs
@@ -9,11 +9,20 @@ use wasm_bindgen::prelude::*;
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-extern {
-    fn alert(s: &str);
+#[repr(u8)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum Cell {
+    Dead = 0,
+    Alive = 1,
 }
-
 #[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, wasm-game-of-life!");
+pub struct Universe {
+    width: u32,
+    height: u32,
+    cells: Vec<Cell>,
+}
+impl Universe {
+    fn get_index(&self, row: u32, column: u32) -> usize {
+        (row * self.width + column) as usize;
+    }
 }
